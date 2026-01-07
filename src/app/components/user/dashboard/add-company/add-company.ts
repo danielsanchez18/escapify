c14ef044f874f37289b2a8490d8d2a32da5c4282
danielsanchez18 <dsanchez151r@gmail.com>
2026-01-06 22:50:45 -0500

    feat: enhance add company form with improved structure and styling

--- src/app/components/user/dashboard/add-company/add-company.ts
+++ src/app/components/user/dashboard/add-company/add-company.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Building2, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'component-user-dashboard-add-company',
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    LucideAngularModule
   ],
   templateUrl: './add-company.html',
 })
-export class ComponentUserDashboardAddCompany { }
+export class ComponentUserDashboardAddCompany {
+
+  readonly Building2 = Building2;
+
+}
